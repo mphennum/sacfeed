311eeb670759f7aa0df0d57f6ac0ad848bed4a17
Max Hennum <mphennum@gmail.com>
2014-10-25 20:10:37 +0000

    correct origin header check

--- lib/App.php
+++ lib/App.php
@@ -28,23 +28,12 @@ abstract class App {
 		$opts = [];
 
 		$opts['host'] = ($_SERVER['HTTP_HOST'] === Config::APIHOST) ? self::API : self::WWW;
-		//$opts['ip'] = isset($_COOKIE['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
-		//$opts['agent'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
 		$opts['origin'] = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
-		$opts['referer'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
 		$opts['method'] = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : null;
-		$opts['uri'] = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : null;
-		$opts['sid'] = isset($_COOKIE['sid']) ? $_COOKIE['sid'] : null;
-
-		if ($opts['method'] === 'POST' || $opts['method'] === 'PUT') {
-			$opts['params'] = &$_POST;
-		} else {
-			$opts['params'] = &$_GET;
-		}
 
 		if ($opts['host'] === self::API) {
 			$origin = isset($opts['origin']) ? $opts['origin'] : null;
-			if ($origin !== Config::WWWHOST) {
+			if ($origin !== 'http://' . Config::WWWHOST) {
 				// return an error
 				exit(0);
 			}
@@ -56,6 +45,18 @@ abstract class App {
 				header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 				exit(0);
 			}
+		}
+
+		//$opts['ip'] = isset($_COOKIE['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
+		//$opts['agent'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
+		$opts['referer'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+		$opts['uri'] = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : null;
+		$opts['sid'] = isset($_COOKIE['sid']) ? $_COOKIE['sid'] : null;
+
+		if ($opts['method'] === 'POST' || $opts['method'] === 'PUT') {
+			$opts['params'] = &$_POST;
+		} else {
+			$opts['params'] = &$_GET;
 		}
 
 		$request = Request::factory($opts);
