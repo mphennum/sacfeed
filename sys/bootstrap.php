b431b1d9bc288a3423b8741579c2a49944b5eeb8
Max Hennum <mphennum@gmail.com>
2014-10-26 20:32:32 +0000

    added cache, database, and cli to bootstrap

--- sys/bootstrap.php
+++ sys/bootstrap.php
@@ -4,7 +4,13 @@ namespace Sacfeed;
 
 require __DIR__ . '/../conf/Config.php';
 require __DIR__ . '/../lib/App.php';
+require __DIR__ . '/../lib/Cache.php';
+require __DIR__ . '/../lib/Database.php';
 require __DIR__ . '/../lib/Request.php';
 require __DIR__ . '/../lib/Response.php';
+
+if (PHP_SAPI === 'cli') {
+	require __DIR__ . '/../lib/CLI.php';
+}
 
 App::init();
