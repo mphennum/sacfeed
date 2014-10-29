aa388afdd644f00f377810a6c40dad4648e329f4
Max Hennum <mphennum@gmail.com>
2014-10-29 02:25:22 +0000

    content type header for json

--- tmpl/api/json.php
+++ tmpl/api/json.php
@@ -2,6 +2,8 @@
 
 namespace Sacfeed;
 
+header('Content-Type: application/json; charset=UTF-8');
+
 if ($status['code'] !== 200) {
 	echo json_encode($status, Config::DEVMODE ? JSON_PRETTY_PRINT : 0);
 } else if (empty($result)) {
