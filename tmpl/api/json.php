d6d755ae469309716aac0ef708947cf64be757b0
Max Hennum <mphennum@gmail.com>
2014-11-01 16:12:42 +0000

    json response format update

--- tmpl/api/json.php
+++ tmpl/api/json.php
@@ -4,10 +4,8 @@ namespace Sacfeed;
 
 header('Content-Type: application/json; charset=UTF-8');
 
-if ($status['code'] !== 200) {
-	echo json_encode($status, Config::DEVMODE ? JSON_PRETTY_PRINT : 0);
-} else if (empty($result)) {
-	echo '{}';
-} else {
-	echo json_encode($result, Config::DEVMODE ? JSON_PRETTY_PRINT : 0);
+if ($response !== null) {
+	echo empty($response) ? '{}' : json_encode($response, Config::DEVMODE ? JSON_PRETTY_PRINT : 0);
 }
+
+echo "\n";
