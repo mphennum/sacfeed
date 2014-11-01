b01003ef33c69c5bbd96240a98302771b8ba872e
Max Hennum <mphennum@gmail.com>
2014-11-01 18:20:23 +0000

    major improvements for database class

--- lib/Database.php
+++ lib/Database.php
@@ -23,39 +23,113 @@ abstract class Database {
 		self::$mongo->wtimeout = 30000;
 	}
 
-	// queued commands
+	// commands
 
-	static public function batchInsert($collection, $records) {
-		self::$queue[] = [
-			'type' => self::BATCH,
-			'collection' => $collection,
-			'records' => $records
-		];
+	static public function findOne() {
+
 	}
 
-	static public function insert($collection, $record) {
-		self::$queue[] = [
-			'type' => self::INSERT,
-			'collection' => $collection,
-			'record' => $record
-		];
+	static public function find() {
+
 	}
 
-	static public function update($collection, $where, $record) {
-		self::$queue[] = [
-			'type' => self::UPDATE,
-			'collection' => $collection,
-			'where' => $where,
-			'record' => $record
-		];
+	static public function distinct() {
+
 	}
 
-	static public function remove($collection, $where) {
-		self::$queue[] = [
-			'type' => self::REMOVE,
-			'collection' => $collection,
-			'where' => $where
-		];
+	static public function batchInsert($collection, $records = [], $w = 0) {
+		if (empty($records)) {
+			return;
+		}
+
+		$seen = [];
+		foreach ($records as $record) {
+			$id = &$record['_id'];
+			if (isset($seen[$id])) {
+				throw new Exception('Duplicate ID found in batch insert');
+			}
+
+			$seen[$id] = true;
+		}
+
+		if ($w === 0) {
+			self::$queue[] = [
+				'type' => self::BATCH,
+				'collection' => $collection,
+				'records' => $records,
+				'w' => 0
+			];
+
+			return;
+		}
+
+		if ($w === false) {
+			$w = 0;
+		}
+
+		self::$mongo->$collection->batchInsert($records, ['continueOnError' => true, 'w' => $w]);
+	}
+
+	static public function insert($collection, $record = [], $w = 0) {
+		if (empty($record)) {
+			return;
+		}
+
+		if ($w === 0) {
+			self::$queue[] = [
+				'type' => self::INSERT,
+				'collection' => $collection,
+				'record' => $record
+			];
+
+			return;
+		}
+
+		if ($w === false) {
+			$w = 0;
+		}
+
+		self::$mongo->$collection->insert($record, ['w' => $w]);
+	}
+
+	static public function update($collection, $where = [], $record = [], $w = 0, $multi = false) {
+		if (empty($record)) {
+			return;
+		}
+
+		if ($w === 0) {
+			self::$queue[] = [
+				'type' => self::UPDATE,
+				'collection' => $collection,
+				'where' => $where,
+				'record' => $record,
+				'multi' => $multi
+			];
+
+			return;
+		}
+
+		self::$mongo->$collection->update($where, $record, ['w' => $w, 'multiple' => $multi]);
+	}
+
+	static public function remove($collection, $where = [], $w = 0, $multi = false) {
+		if ($w === 0) {
+			self::$queue[] = [
+				'type' => self::REMOVE,
+				'collection' => $collection,
+				'where' => $where,
+				'w' => $w,
+				'multi' => $multi
+			];
+
+			return;
+		}
+
+		if ($w === false) {
+			$w = 0;
+		}
+
+		self::$mongo->$collection->remove($where, ['w' => $w, 'justOne' => !$multi]);
 	}
 
 	static public function shutdown() {
@@ -63,6 +137,8 @@ abstract class Database {
 		foreach (self::$queue as $command) {
 			$type = &$command['type'];
 			$collection = &$command['collection'];
+
+			// auto batch
 
 			if ($type === self::INSERT) {
 				if (!isset($batches[$collection])) {
@@ -82,26 +158,26 @@ abstract class Database {
 				continue;
 			}
 
-			$collection = self::$mongo->$$collection;
-			$where = &$command['where'];
+			// update / remove
 
 			if ($type === self::UPDATE) {
-				$collection->update($where, $command['record'], ['w' => 0]);
+				self::update($collection, $command['where'], $command['record'], false, $command['multi']);
 			} else if ($type === self::REMOVE) {
-				$collection->remove($where, ['w' => 0]);
+				self::remove($collection, $command['where'], false, $command['multi']);
 			}
 		}
+
+		// batches
 
 		foreach ($batches as $collection => $batch) {
 			if (empty($batch)) {
 				continue;
 			}
 
-			$collection = self::$mongo->$$collection;
 			if (count($batch) === 1) {
-				$collection->insert($batch[0], ['w' => 0]);
+				self::insert($collection, $batch[0], false);
 			} else {
-				$collection->batchInsert($batch, ['continueOnError' => true, 'w' => 0]);
+				self::batchInsert($collection, $batch, false);
 			}
 		}
 	}
