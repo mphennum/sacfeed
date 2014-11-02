b9b3181a8be909f50fc0d1e4e63eb09ff5d047e8
Max Hennum <mphennum@gmail.com>
2014-11-02 23:43:08 +0000

    meta tags, conent type for html templates

--- tmpl/www/article.php
+++ tmpl/www/article.php
@@ -1,7 +1,11 @@
+<?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>live sacbee.com article feed - sacfeed</title>
 
