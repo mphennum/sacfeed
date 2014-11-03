237a30742b0f6b26f377e358243b016e0f08b010
Max Hennum <mphennum@gmail.com>
2014-11-03 16:47:58 +0000

    articles tmpl outputs sections and articles

--- tmpl/www/article.php
+++ tmpl/www/article.php
@@ -1,19 +1,52 @@
-<?php header('Content-Type: text/html; charset=UTF-8'); ?>
+<?php
+
+namespace Sacfeed;
+
+header('Content-Type: text/html; charset=UTF-8');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
 
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<? include(__DIR__ . '/ssi/meta.php'); ?>
 
 <title>live sacbee.com article feed - sacfeed</title>
+
+<? include(__DIR__ . '/ssi/styles.php'); ?>
 
 </head>
 
 <body>
 
-<p>articles</p>
+<header><a href="http://<?= Config::WWWHOST ?>/">sacfeed</a></header>
+
+<nav>
+<?
+foreach ($response['sections'] as $section) {
+	echo '<a href="http://', Config::WWWHOST, $section['id'], '">', $section['name'], '</a>';
+}
+?>
+</nav>
+
+<main>
+<?
+foreach ($response['articles'] as $article) {
+	echo '<article>';
+	if ($article['thumb']) {
+		echo '<img src="', $article['thumb'], '" alt="', $article['thumb'], '">';
+	}
+
+	echo '<h2>', $article['title'], '</h2>';
+	echo '<p>', $article['author'], '</p>';
+	echo '<p>', $article['summary'], '</p>';
+	echo '</article>', "\n";
+}
+?>
+</main>
+
+<? include(__DIR__ . '/ssi/scripts.php'); ?>
 
 </body>
 
