4a56e6c74c971ee2ced93a48b2c876fed2bd9413
Max Hennum <mphennum@gmail.com>
2014-11-03 06:40:50 +0000

    set json fields method for db articles

--- lib/db/Article.php
+++ lib/db/Article.php
@@ -2,6 +2,8 @@
 
 namespace Sacfeed\DB;
 
+use MongoDate;
+
 use Sacfeed\Database;
 
 class Article extends Record {
@@ -35,12 +37,42 @@ class Article extends Record {
 			'content' => $this->fields['content'],
 			'summary' => $this->fields['summary'],
 			'url' => $this->fields['url'],
-			'ts' => $this->fields['ts']
+			'ts' => $this->fields['ts']->sec * 1000 + ($this->fields['ts']->usec / 1000)
 		];
 	}
 
-	public function setJSONFields($json) {
+	public function setJSONFields($section, $json) {
+		$this->fields['_id'] = (int) $json['id'];
+		$this->fields['section'] = $section;
+		$this->fields['title'] = isset($json['title']) ? $json['title'] : null;
+		$this->fields['subtitle'] = isset($json['sub_headline']) ? $json['sub_headline'] : null;
+		$this->fields['author'] = isset($json['author']) ? $json['author'] : null;
+		$this->fields['content'] = isset($json['content']) ? $json['content'] : null;
+		$this->fields['summary'] = isset($json['summary']) ? $json['summary'] : null;
+		$this->fields['url'] = isset($json['url']) ? $json['url'] : null;
 
+		if (isset($json['pub_date'])) {
+			$ts = (int) $json['pub_date'];
+			$this->fields['ts'] = new MongoDate($ts / 1000);
+		} else {
+			$this->fields['ts'] = new MongoDate();
+		}
+
+		if (isset($json['assets']['Photo'][0])) {
+			$photo = $json['assets']['Photo'][0];
+			if (isset($photo['thumb_url']) && trim($photo['thumb_url'])) {
+				$this->fields['thumb'] = $photo['thumb_url'];
+			} else {
+				$this->fields['thumb'] = $photo['url'];
+			}
+		} else if (isset($json['assets']['Gallery'][0]['images'][0])) {
+			$photo = $json['assets']['Gallery'][0]['images'][0];
+			if (isset($photo['thumb_url']) && trim($photo['thumb_url'])) {
+				$this->fields['thumb'] = $photo['thumb_url'];
+			} else {
+				$this->fields['thumb'] = $photo['url'];
+			}
+		}
 	}
 
 	static public function find($query = [], $projection = []) {
