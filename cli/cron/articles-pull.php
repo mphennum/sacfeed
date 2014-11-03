654deff1d99d37ffad5d06d405009e21d25f7335
Max Hennum <mphennum@gmail.com>
2014-11-03 06:41:35 +0000

    finished articles pull cli

--- cli/cron/articles-pull.php
+++ cli/cron/articles-pull.php
@@ -13,5 +13,48 @@ require __DIR__ . '/../../sys/bootstrap.php';
 CLI::init('Sacfeed -- pull new articles cli');
 
 $ts = new MongoDate();
-$ts->sec -= 3600;
-$cursor = Section::find(['ts' => ['$gt' => $ts]]);
+$ts->sec -= 900;
+
+$i = 0;
+$seen = [];
+$cursor = Section::find(['ts' => ['$gt' => $ts]], ['_id' => 1]);
+foreach ($cursor as $record) {
+	$section = $record['_id'];
+
+	$url = 'http://' . Config::SACBEEHOST . $section . Config::JSONQUERY;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$json = curl_exec($ch);
+	$info = curl_getinfo($ch);
+
+	if ($json === false || trim($json) === '') {
+		CLI::error('curl failed: ' . $url);
+	}
+
+	CLI::message($url);
+	$json = json_decode($json, true);
+
+	foreach ($json['items'] as $item) {
+		$id = (int) $item['id'];
+
+		if (isset($seen[$id])) {
+			continue;
+		}
+
+		$seen[$id] = true;
+
+		$article = new Article();
+		if ($article->findOne($id)) {
+			continue;
+		}
+
+		$article->setJSONFields($section, $item);
+		$article->insert();
+
+		CLI::message($article->title);
+		++$i;
+	}
+}
+
+CLI::notice($i . ' articles inserted');
