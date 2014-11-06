fcb27994d61b25f563cea26d9c1f2d45ce7b1806
Max Hennum <mphennum@gmail.com>
2014-11-06 23:55:15 +0000

    started js files

---
+++ js/src/sacfeed.js
@@ -0,0 +1,23 @@
+(function() {
+
+if (window.sacfeed) {
+	return;
+}
+
+var sacfeed = window.sacfeed || {};
+
+sacfeed.init = function() {
+	delete sacfeed.init;
+};
+
+sacfeed.request = function() {
+
+};
+
+sacfeed.load = function() {
+
+};
+
+sacfeed.init();
+
+})();
