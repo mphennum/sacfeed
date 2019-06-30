b3c6de3b954ddd4c82cdf2180274d0f7348d24dd
Max Hennum <mphennum@gmail.com>
2019-06-30 13:19:10 -0700

    article static get thumb from assets method

--- lib/db/Article.php
+++ lib/db/Article.php
@@ -4,7 +4,7 @@ namespace Sacfeed\DB;
 
 use MongoDB\BSON\UTCDateTime as MongoDateTime;
 
-use Sacfeed\CLI;
+use Sacfeed\Config;
 use Sacfeed\Database;
 
 class Article extends Record {
@@ -60,33 +60,45 @@ class Article extends Record {
 			$this->fields['ts'] = new MongoDateTime();
 		}
 
+		if ($json['thumb'] === null) {
+			$this->fields['thumb'] = self::getThumbFromAssets($json);
+		} else {
+			$this->fields['thumb'] = $json['thumb'];
+		}
+	}
+
+	static public function getThumbFromAssets($json) {
+		$thumb = null;
+
 		if (isset($json['assets']['Photo'][0])) {
 			$photo = $json['assets']['Photo'][0];
 			if (isset($photo['thumb_url']) && trim($photo['thumb_url'])) {
-				$this->fields['thumb'] = $photo['thumb_url'];
+				$thumb = $photo['thumb_url'];
 			} else {
-				$this->fields['thumb'] = $photo['url'];
+				$thumb = $photo['url'];
 			}
 		} else if (isset($json['assets']['Gallery'][0]['images'][0])) {
 			$photo = $json['assets']['Gallery'][0]['images'][0];
 			if (isset($photo['thumb_url']) && trim($photo['thumb_url'])) {
-				$this->fields['thumb'] = $photo['thumb_url'];
+				$thumb = $photo['thumb_url'];
 			} else {
-				$this->fields['thumb'] = $photo['url'];
+				$thumb = $photo['url'];
 			}
 		}
 
-		if ($this->fields['thumb'] !== null) {
-			$url = preg_replace('/\/FREE_[0-9]+\//', '/LANDSCAPE_320/', $this->fields['thumb']);
+		if ($thumb !== null) {
+			$url = preg_replace('/\/FREE_[0-9]+\//', '/LANDSCAPE_320/', $thumb);
 
 			$ch = curl_init($url);
 			curl_setopt_array($ch, [
-				CURLOPT_NOBODY => true,
+				CURLOPT_URL => $url,
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_NOBODY => true,
+				CURLOPT_USERAGENT => Config::CURLUSERAGENT,
+				CURLOPT_TIMEOUT => 5,
 				CURLOPT_MAXREDIRS => 5,
-				CURLOPT_TIMEOUT => 15,
-				CURLOPT_USERAGENT => 'curl/7.29.0',
+				CURLOPT_HTTPHEADER => Config::$curlHeaders,
 			]);
 
 			$header = curl_exec($ch);
@@ -94,9 +106,11 @@ class Article extends Record {
 			curl_close($ch);
 
 			if ($header !== false && $info['http_code'] === 200) {
-				$this->fields['thumb'] = $url;
+				return $url;
 			}
 		}
+
+		return $thumb;
 	}
 
 	static public function find(array $query = [], array $projection = null, array $sort = null, $limit = null) {
